13371a24992bac084d628632330d0a21eb369757
ヒューマンワーキン <coolxiaom95@gmail.com>
2022-12-19 12:06:41 +0800

    Update LoreUpdateEvent.java

--- src/main/java/think/rpgitems/event/LoreUpdateEvent.java
+++ src/main/java/think/rpgitems/event/LoreUpdateEvent.java
@@ -7,17 +7,18 @@ import org.bukkit.event.HandlerList;
 import java.util.List;
 
 public class LoreUpdateEvent extends Event {
-    public static final HandlerList handlerList = new HandlerList();
+    private static final HandlerList handlers = new HandlerList();
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
     public List<String> oldLore;
     public List<String> newLore;
 
     public LoreUpdateEvent(List<String> oldLore, List<String> newLore) {
         this.oldLore = oldLore;
         this.newLore = newLore;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return handlerList;
     }
 }

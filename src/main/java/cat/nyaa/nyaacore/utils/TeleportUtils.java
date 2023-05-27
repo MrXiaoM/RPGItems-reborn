c4e53ff5f64dd57b2ad2e57e20d66dec2bbd2cea
ヒューマンワーキン <coolxiaom95@gmail.com>
2023-05-27 19:58:08 +0800

    remove Essentials support

--- src/main/java/cat/nyaa/nyaacore/utils/TeleportUtils.java
+++ src/main/java/cat/nyaa/nyaacore/utils/TeleportUtils.java
@@ -1,6 +1,5 @@
 package cat.nyaa.nyaacore.utils;
 
-import com.earth2me.essentials.Essentials;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -14,19 +13,9 @@ public final class TeleportUtils {
         if (!player.isOnline() || loc == null || loc.getWorld() == null) {
             return false;
         }
-        Essentials ess = (Essentials) Bukkit.getServer().getPluginManager().getPlugin("Essentials");
-        if (ess != null) {
-            try {
-                ess.getUser(player).getTeleport().now(loc, false, PlayerTeleportEvent.TeleportCause.PLUGIN);
-                return true;
-            } catch (Exception e) {
-                return false;
-            }
-        } else {
-            player.setFallDistance(0);
-            player.teleport(loc, PlayerTeleportEvent.TeleportCause.PLUGIN);
-            return true;
-        }
+        player.setFallDistance(0);
+        player.teleport(loc, PlayerTeleportEvent.TeleportCause.PLUGIN);
+        return true;
     }
 
     public static void Teleport(List<Player> players, Location loc) {

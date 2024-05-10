31cb6af18fd1f315d84f5ab444a36602e1a812ed
MrXiaoM <coolxiaom95@gmail.com>
2024-05-10 18:16:09 +0800

    add mythic skill power

---
+++ src/main/java/think/rpgitems/power/impl/Mythic.java
@@ -0,0 +1,92 @@
+/*
+ *  This file is part of RPG Items.
+ *
+ *  RPG Items is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  RPG Items is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RPG Items.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package think.rpgitems.power.impl;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import think.rpgitems.I18n;
+import think.rpgitems.power.*;
+import think.rpgitems.support.MythicSupport;
+
+import static think.rpgitems.power.Utils.checkCooldown;
+
+@Meta(defaultTrigger = "RIGHT_CLICK", generalInterface = PowerPlain.class, implClass = Mythic.Impl.class)
+public class Mythic extends BasePower {
+    @Property(order = 1)
+    public int cooldown = 0;
+
+    @Property
+    public int cost = 0;
+
+    @Property(required = true)
+    public String skill;
+
+    /**
+     * Cooldown time of this power
+     */
+    public int getCooldown() {
+        return cooldown;
+    }
+
+    /**
+     * Cost of this power
+     */
+    public int getCost() {
+        return cost;
+    }
+
+    @Override
+    public String getName() {
+        return "mythic";
+    }
+
+    @Override
+    public String displayText() {
+        return I18n.formatDefault("power.mythic", skill);
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public class Impl implements PowerPlain, PowerRightClick, PowerLeftClick {
+        @Override
+        public PowerResult<Void> rightClick(final Player player, ItemStack stack, PlayerInteractEvent event) {
+            return fire(player, stack);
+        }
+
+        public PowerResult<Void> fire(final Player player, ItemStack s) {
+            if (!checkCooldown(getPower(), player, getCooldown(), false, true)) return PowerResult.cd();
+            if (!getItem().consumeDurability(s, getCost())) return PowerResult.cost();
+
+            boolean result = MythicSupport.castSkill(player, getSkill());
+
+            return result ? PowerResult.ok() : PowerResult.fail();
+        }
+
+        @Override
+        public Power getPower() {
+            return Mythic.this;
+        }
+
+        @Override
+        public PowerResult<Void> leftClick(final Player player, ItemStack stack, PlayerInteractEvent event) {
+            return fire(player, stack);
+        }
+    }
+}
--- src/main/java/think/rpgitems/support/MythicSupport.java
+++ src/main/java/think/rpgitems/support/MythicSupport.java
@@ -1,12 +1,27 @@
 package think.rpgitems.support;
 
+import io.lumine.mythic.bukkit.MythicBukkit;
+import io.lumine.mythic.core.utils.MythicUtil;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.persistence.PersistentDataType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MythicSupport {
-    private static final NamespacedKey type = new NamespacedKey("MythicMobs".toLowerCase(), "type");;
+    private static final NamespacedKey type = new NamespacedKey("MythicMobs".toLowerCase(), "type");
     public static boolean isMythic(Entity entity) {
         return entity.getPersistentDataContainer().has(type, PersistentDataType.STRING);
+    }
+
+    public static boolean castSkill(Player player, String spell) {
+        LivingEntity target = MythicUtil.getTargetedEntity(player);
+        List<Entity> targets = new ArrayList<>();
+        targets.add(target);
+        return MythicBukkit.inst().getAPIHelper().castSkill(player, spell, player, player.getLocation(), targets, null, 1.0F);
+
     }
 }

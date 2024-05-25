bdbc3b160afc7d8fa11c996bde82f48b8f0696d2
MrXiaoM <coolxiaom95@gmail.com>
2024-05-25 18:40:29 +0800

    get NMS instance using reflection

--- src/main/java/think/rpgitems/utils/nms/NMS.java
+++ src/main/java/think/rpgitems/utils/nms/NMS.java
@@ -4,32 +4,9 @@ import org.bukkit.Bukkit;
 import think.rpgitems.utils.nms.legacy.LegacyEntityTools;
 import think.rpgitems.utils.nms.legacy.LegacyNBTTagTools;
 import think.rpgitems.utils.nms.legacy.LegacyStackTools;
-import think.rpgitems.utils.nms.v1_17_R1.EntityTools_v1_17_R1;
-import think.rpgitems.utils.nms.v1_17_R1.NBTTagTools_v1_17_R1;
-import think.rpgitems.utils.nms.v1_17_R1.StackTools_v1_17_R1;
-import think.rpgitems.utils.nms.v1_18_R1.EntityTools_v1_18_R1;
-import think.rpgitems.utils.nms.v1_18_R1.NBTTagTools_v1_18_R1;
-import think.rpgitems.utils.nms.v1_18_R1.StackTools_v1_18_R1;
-import think.rpgitems.utils.nms.v1_18_R2.EntityTools_v1_18_R2;
-import think.rpgitems.utils.nms.v1_18_R2.NBTTagTools_v1_18_R2;
-import think.rpgitems.utils.nms.v1_18_R2.StackTools_v1_18_R2;
-import think.rpgitems.utils.nms.v1_19_R3.EntityTools_v1_19_R3;
-import think.rpgitems.utils.nms.v1_19_R3.NBTTagTools_v1_19_R3;
-import think.rpgitems.utils.nms.v1_19_R3.StackTools_v1_19_R3;
-import think.rpgitems.utils.nms.v1_20_R1.EntityTools_v1_20_R1;
-import think.rpgitems.utils.nms.v1_20_R1.NBTTagTools_v1_20_R1;
-import think.rpgitems.utils.nms.v1_20_R1.StackTools_v1_20_R1;
-import think.rpgitems.utils.nms.v1_20_R2.EntityTools_v1_20_R2;
-import think.rpgitems.utils.nms.v1_20_R2.NBTTagTools_v1_20_R2;
-import think.rpgitems.utils.nms.v1_20_R2.StackTools_v1_20_R2;
-import think.rpgitems.utils.nms.v1_20_R3.EntityTools_v1_20_R3;
-import think.rpgitems.utils.nms.v1_20_R3.NBTTagTools_v1_20_R3;
-import think.rpgitems.utils.nms.v1_20_R3.StackTools_v1_20_R3;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.Map;
-import java.util.TreeMap;
 import java.util.logging.Logger;
 
 public class NMS {
@@ -39,43 +16,6 @@ public class NMS {
     INBTTagTools nbtTools;
     private static String versionString;
     private static NMS inst = null;
-    private final Map<String, Runnable> versions = new TreeMap<>(String.CASE_INSENSITIVE_ORDER) {{
-        put("v1_17_R1", () -> {
-            stackTools = new StackTools_v1_17_R1();
-            entityTools = new EntityTools_v1_17_R1();
-            nbtTools = new NBTTagTools_v1_17_R1();
-        });
-        put("v1_18_R1", () -> {
-            stackTools = new StackTools_v1_18_R1();
-            entityTools = new EntityTools_v1_18_R1();
-            nbtTools = new NBTTagTools_v1_18_R1();
-        });
-        put("v1_18_R2", () -> {
-            stackTools = new StackTools_v1_18_R2();
-            entityTools = new EntityTools_v1_18_R2();
-            nbtTools = new NBTTagTools_v1_18_R2();
-        });
-        put("v1_19_R3", () -> {
-            stackTools = new StackTools_v1_19_R3();
-            entityTools = new EntityTools_v1_19_R3();
-            nbtTools = new NBTTagTools_v1_19_R3();
-        });
-        put("v1_20_R1", () -> {
-            stackTools = new StackTools_v1_20_R1();
-            entityTools = new EntityTools_v1_20_R1();
-            nbtTools = new NBTTagTools_v1_20_R1();
-        });
-        put("v1_20_R2", () -> {
-            stackTools = new StackTools_v1_20_R2();
-            entityTools = new EntityTools_v1_20_R2();
-            nbtTools = new NBTTagTools_v1_20_R2();
-        });
-        put("v1_20_R3", () -> {
-            stackTools = new StackTools_v1_20_R3();
-            entityTools = new EntityTools_v1_20_R3();
-            nbtTools = new NBTTagTools_v1_20_R3();
-        });
-    }};
     private NMS(Logger logger) {
         this.logger = logger;
     }
@@ -88,10 +28,9 @@ public class NMS {
     public boolean init() {
         try {
             String v = getVersion();
-            Runnable init = versions.get(v);
-            if (init != null) {
-                init.run();
+            if (load(v)) {
                 logger.info("Your server version " + v + " is supported! Enjoy!");
+                return true;
             } else {
                 logger.warning("Your server version " + v + " is not supported!");
                 logger.warning("Now you are running in Legacy Mode. Some functions may not work.");
@@ -99,7 +38,6 @@ public class NMS {
                 entityTools = new LegacyEntityTools();
                 nbtTools = new LegacyNBTTagTools(); // TODO: 找个库来处理 nbt
             }
-            return init != null;
         } catch (Throwable t) {
             StringWriter sw = new StringWriter();
             try (PrintWriter pw = new PrintWriter(sw)) {
@@ -108,6 +46,21 @@ public class NMS {
             logger.warning(sw.toString());
         }
         return false;
+    }
+
+    public boolean load(String nms) throws ReflectiveOperationException {
+        try {
+            String root = "think.rpgitems.utils.nms." + nms + ".";
+            Class<?> classStackTools = Class.forName(root + "StackTools_" + nms);
+            Class<?> classEntityTools = Class.forName(root + "EntityTools_" + nms);
+            Class<?> classNBTTagTools = Class.forName(root + "NBTTagTools_" + nms);
+            stackTools = (IStackTools) classStackTools.getConstructor().newInstance();
+            entityTools = (IEntityTools) classEntityTools.getConstructor().newInstance();
+            nbtTools = (INBTTagTools) classNBTTagTools.getConstructor().newInstance();
+            return true;
+        } catch (ReflectiveOperationException ignored) {
+            return false;
+        }
     }
 
     public static IStackTools stackTools() {
@@ -122,12 +75,6 @@ public class NMS {
         return inst.nbtTools;
     }
 
-    public static boolean starts(String v, String... versions) {
-        for (String s : versions) {
-            if (v.startsWith(s)) return true;
-        }
-        return false;
-    }
     public static boolean equals(String v, String... versions) {
         for (String s : versions) {
             if (v.equals(s)) return true;

7094bbe4e60de65d39ab483dffa9ef132acf2a75
MrXiaoM <coolxiaom95@gmail.com>
2024-07-01 08:25:57 +0800

    add untested legacy method

--- src/main/java/think/rpgitems/utils/nms/legacy/LegacyEntityTools.java
+++ src/main/java/think/rpgitems/utils/nms/legacy/LegacyEntityTools.java
@@ -1,11 +1,28 @@
 package think.rpgitems.utils.nms.legacy;
 
+import de.tr7zw.changeme.nbtapi.NBT;
+import de.tr7zw.changeme.nbtapi.iface.ReadWriteNBT;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import think.rpgitems.utils.nms.IEntityTools;
 
+/**
+ * 备用的设置实体NBT方法，未测试是否可用
+ */
 public class LegacyEntityTools implements IEntityTools {
     @Override
     public void setEntityTag(Entity e, String tag) {
-        // do nothing
+        if (e instanceof Player) {
+            throw new IllegalArgumentException("Player NBT cannot be edited");
+        }
+        ReadWriteNBT nbtToBeMerged;
+        try {
+            nbtToBeMerged = NBT.parseNBT(tag);
+        } catch (Exception ex) {
+            throw new IllegalArgumentException("Invalid NBTTag string", ex);
+        }
+        NBT.modify(e, it -> {
+            it.mergeCompound(nbtToBeMerged);
+        });
     }
 }

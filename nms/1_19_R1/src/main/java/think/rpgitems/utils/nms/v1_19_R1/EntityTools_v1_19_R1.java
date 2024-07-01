f67f89e4d37b68886211e433372eb51262dae1f2
MrXiaoM <coolxiaom95@gmail.com>
2024-07-01 08:42:43 +0800

    supports v1_19_R1 and v1_19_R2

---
+++ nms/1_19_R1/src/main/java/think/rpgitems/utils/nms/v1_19_R1/EntityTools_v1_19_R1.java
@@ -0,0 +1,40 @@
+package think.rpgitems.utils.nms.v1_19_R1;
+
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.advancements.critereon.CriterionConditionNBT;
+import net.minecraft.nbt.MojangsonParser;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.server.level.EntityPlayer;
+import org.bukkit.craftbukkit.v1_19_R1.entity.CraftEntity;
+import org.bukkit.entity.Entity;
+
+import java.util.UUID;
+
+import think.rpgitems.utils.nms.IEntityTools;
+
+public class EntityTools_v1_19_R1 implements IEntityTools {
+    @Override
+    public void setEntityTag(Entity e, String tag) {
+        net.minecraft.world.entity.Entity nmsEntity = ((CraftEntity) e).getHandle();
+
+        if (nmsEntity instanceof EntityPlayer) {
+            throw new IllegalArgumentException("Player NBT cannot be edited");
+        } else {
+            NBTTagCompound nbtToBeMerged;
+
+            try {
+                nbtToBeMerged = MojangsonParser.a(tag);
+            } catch (CommandSyntaxException ex) {
+                throw new IllegalArgumentException("Invalid NBTTag string");
+            }
+            NBTTagCompound nmsOrigNBT = CriterionConditionNBT.b(nmsEntity); // entity to nbt
+            NBTTagCompound nmsClonedNBT = nmsOrigNBT.h(); // clone
+            nmsClonedNBT.a(nbtToBeMerged); // merge NBT
+            if (!nmsClonedNBT.equals(nmsOrigNBT)) {
+                UUID uuid = nmsEntity.cp(); // store UUID
+                nmsEntity.g(nmsClonedNBT); // set nbt
+                nmsEntity.a_(uuid); // set uuid
+            }
+        }
+    }
+}
